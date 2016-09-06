09709dff0392859618240fd39e0f571acefcdded
212572063 <justin.le@ge.com>
2016-09-06 10:20:42 -0700

    added i18n localeSettings

---
+++ i18n/localeSettings.js
@@ -0,0 +1,20 @@
+var locale = (window.navigator.languages) ? window.navigator.languages[0]
+              : (window.navigator.userLanguage||window.navigator.language);//gets user's preffered language as dictated in settings
+var supportedLocales = ['en','en-US'];
+var defaultLocale = 'en';
+var localeScript = document.createElement("script");
+localeScript.type = "application/javascript";
+var scriptName ="modules/wip/i18n/wip-";
+//locale = 'te-st';
+if(!supportedLocales.includes(locale)){
+  if(supportedLocales.includes(locale.substring(2))){
+    //set to generic locale e.g. en-US becomes en
+      scriptName += locale.substring(2) + ".js";
+  } else{
+      scriptName += defaultLocale + ".js";
+  }
+} else{
+  scriptName += locale + ".js";
+}
+localeScript.src = scriptName.toLowerCase();
+document.body.appendChild(localeScript);
