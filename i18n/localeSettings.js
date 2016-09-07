132d5093786a8ce097e4c6e4e115458b936e13be
212572063 <justin.le@ge.com>
2016-09-07 16:48:20 -0700

    refactoring i18n, changed localeSettings script

--- i18n/localeSettings.js
+++ i18n/localeSettings.js
@@ -1,20 +1,50 @@
 var locale = (window.navigator.languages) ? window.navigator.languages[0]
               : (window.navigator.userLanguage||window.navigator.language);//gets user's preffered language as dictated in settings
 var supportedLocales = ['en','en-US','es-ES', 'fr-FR'];
-var defaultLocale = 'en';
-var localeScript = document.createElement("script");
-localeScript.type = "application/javascript";
-var scriptName ="modules/i18n/wip-";
+var defaultScript = 'modules/i18n/default.properties';
+var scriptName ="modules/i18n/";
 //locale = 'te-st';
 if(!supportedLocales.includes(locale)){
   if(supportedLocales.includes(locale.substring(2))){
     //set to generic locale e.g. en-US becomes en
-      scriptName += locale.substring(2) + ".js";
+      locale = locale.substring(0,2);
   } else{
-      scriptName += defaultLocale + ".js";
+      scriptName = defaultScript;
   }
-} else{
-  scriptName += locale + ".js";
 }
-localeScript.src = scriptName.toLowerCase();
-document.body.appendChild(localeScript);
+scriptName += locale;
+scriptName = scriptName.toLowerCase();
+scriptName +=  ".properties";
+
+var localeData;
+var xmlhttp =  new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if(xmlhttp.readyState == XMLHttpRequest.DONE){
+    if(xmlhttp.status === 404){
+      scriptName = defaultScript;
+      xmlhttp.open("GET",scriptName,true);
+      xmlhttp.send();
+    } else{
+      localeData = parseProperties(xmlhttp.responseText);
+      localeData['currentLocale'] = locale;
+      // console.log(properties);
+    }
+  }
+};
+xmlhttp.open("GET", scriptName, true);
+xmlhttp.send();
+
+
+
+
+function parseProperties(properties){
+  var keyValuePairs = properties.split(/$/m);
+  var propertiesObj={};
+  keyValuePairs.forEach(function(line){
+    var entry = line.split("=");
+    if(entry[0] && entry[1]){
+      propertiesObj[entry[0].trim()] = entry[1].trim();
+    }
+  });
+  return propertiesObj;
+}
